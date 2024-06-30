a745381d4d69a909bde984a5e82ddebeef0d265c
Taqi <TaqiRahimi@gmail.com>
2024-06-30 15:41:54 +0430

    display Generes

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,16 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area={"nav"}>
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem bg="pink.300" area={"aside"}>
+        <GridItem area={"aside"}>
           <GenreList />
         </GridItem>
       </Show>
